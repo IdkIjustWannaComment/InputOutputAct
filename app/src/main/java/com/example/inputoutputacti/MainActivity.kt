0302cb898888f10c74e3526b6a6acaf4059d49de
IdkIjustWannaComment <nikhil.govender01@gmail.com>
2026-03-10 16:28:25 +0200

    Added Toast

--- app/src/main/java/com/example/inputoutputacti/MainActivity.kt
+++ app/src/main/java/com/example/inputoutputacti/MainActivity.kt
@@ -11,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.btnClickMe)
+
+        btnClickMe?.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Button Clicked",Toast)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
