ba4dded038d9fa0ff0505c890e501ede74780e41
IdkIjustWannaComment <nikhil.govender01@gmail.com>
2026-03-12 08:55:19 +0200

    Patched errors

--- app/src/main/java/com/example/inputoutputacti/MainActivity.kt
+++ app/src/main/java/com/example/inputoutputacti/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +17,15 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val btnClickMe = findViewById<Button>(R.btnClickMe)
+        val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+        val edtNameInput = findViewById<EditText>(R.id.edtNameInput)
+        val txtHello = findViewById<TextView>(R.id.txtHello)
         //toast to ensure the button works
         btnClickMe?.setOnClickListener {
-            Toast.makeText(this@MainActivity,"Button Clicked",Toast.LENGTH_LONG).Show()
+            Toast.makeText(this@MainActivity,"Button Clicked",Toast.LENGTH_LONG).show()
         }
-            Welcome.txt.Text = "Welcome ${nameTxtField.text}"
-            //welcome text = "welcome + nameTxtField
+            txtHello.text = "Welcome, ${edtNameInput.text}!"
+            //welcome text = "welcome + edtNameInput"
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
